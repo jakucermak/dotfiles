4c210d5bf78c718eaeefd868f821c57a8508a13c
Jakub <jakucermak@gmail.com>
2023-04-27 12:54:39 +0200

    added keybinding to tmux to allow sync multipanel

--- install.sh
+++ install.sh
@@ -32,7 +32,7 @@ else
 fi
 
 # Install NeoVim Plugin Manager
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 
 # Install Oh-My-ZSH
