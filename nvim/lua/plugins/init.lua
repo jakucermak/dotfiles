a092b1b5cd6ea7fa711a88685532f862e086387f
Jakub Čermák <jakub@Jakubs-MacBook-Pro.local>
2024-04-24 22:37:59 +0200

    added yabai and skhd configs

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,11 +1,8 @@
+
+require('plugins.packer')
 require('plugins.treesitter')
 require('plugins.lsp')
 require('plugins.lualine')
 require('plugins.neozoom')
 require('plugins.tint')
 
-vim.cmd [[packadd packer.nvim]]
-
-return require('packer').startup( function(use)
-	use 'wbthomason/packer.nvim'
-end)
